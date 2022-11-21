69ebe64e5d9956ce27f1da60e7b060b09987404d
serrin_sage <serrin.doscher53@myhunter.cuny.edu>
2022-11-21 13:32:15 -0500

    Store container updated with error handling from Toastify

--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -1,10 +1,42 @@
-const Store = () => {
-    
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const Store = ({money, setMoney, potionTotal, setPotionTotal}) => {
+    const message = () => toast.error("NOT ENOUGH MONEY")
+    const message2 = () => toast.error("No more money")
+    const buyPotion = (price) => {
+        if (money - price < 0) {
+            
+            message()
+            return
+        } else if (money === 0) {
+            message2()
+            return (setMoney(price - price))
+        }
+        
+        return (setMoney(money - price), setPotionTotal(potionTotal + 1))
+    }
+
     return (
         <div className="store-container">
             <h1>Welcome to the Store!</h1>
-            <p>You have ___ Monies</p>
-            <button>Buy a potion!</button>
+            <h3>You have {money} Monies</h3>
+            <h3>You have {potionTotal} Potions</h3>
+            <div className="potion-container">
+                <button onClick={() => buyPotion(15)} className="buy-potion">Buy a small potion!</button>
+            </div> 
+            <ToastContainer 
+                position="top-right"
+                autoClose={2000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss={false}
+                draggable
+                pauseOnHover
+                toastStyle={{color: "red", height: "200px"}}
+                 />
         </div>
     )
 }
