2b378aad92247d0e1273b8d706451af0de3dd147
serrin_sage <serrin.doscher53@myhunter.cuny.edu>
2022-11-21 10:03:39 -0500

    added header and navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div className="header">
+            <h1 className="header-title">Pokemon Battle Simulator!</h1>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <a href="" className="link-title">Team</a>
+            <a href="" className="link-title">Battle!</a>
+            <a href="" className="link-title">Store</a>
+        </div>
+    )
+}
+
+export default Navbar
