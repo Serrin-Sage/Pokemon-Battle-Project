7def2aad62e2c0942a81ebab04ef872e989a6679
Vincent <vkl2109@columbia.edu>
2022-11-22 11:40:48 -0500

    Updated stats table styling

--- src/components/TeamResult.jsx
+++ src/components/TeamResult.jsx
@@ -2,6 +2,7 @@ const TeamResult = ({ pokemon }) => {
     return (
         <div className="resultCard"> 
             <img src={pokemon.sprites.front_default}></img>
+            <h3>#{pokemon.id} {pokemon.name}</h3>
         </div>
     )
 }
